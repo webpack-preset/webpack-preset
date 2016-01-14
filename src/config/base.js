afd603f7139de0535513e5ac507a76d7cd54b8a5
Tim Dorr <timdorr@timdorr.com>
2016-01-13 21:00:27 -0500

    Make sure the loaders are set up and ready to go.

--- src/config/base.js
+++ src/config/base.js
@@ -9,6 +9,9 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/'
   },
+  module: {
+    loaders: []
+  },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin()
